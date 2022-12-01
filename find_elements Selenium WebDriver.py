67379fa5d7c2e49d4fc4094e7dcc3aaf98c61dc4
Anton Klennitski <75129039+Warrior-RS@users.noreply.github.com>
2022-12-01 16:13:34 +0100

    Update find_elements Selenium WebDriver.py

--- find_elements Selenium WebDriver.py
+++ find_elements Selenium WebDriver.py
@@ -5,17 +5,17 @@ import time
 try:
     browser = webdriver.Chrome()
     browser.get("http://suninjuly.github.io/registration2.html")
-    elements = browser.find_elements(By.CSS_SELECTOR, "input")
+    elements = browser.find_elements(By.CSS_SELECTOR, "input")  #Looking for all items with selector 'input'
     for element in elements:
-        element.send_keys("text")
+        element.send_keys("text")                               #Add some 'text' to the found fields
 
     button = browser.find_element(By.XPATH, "/html/body/div/form/button")
     button.click()
 
 finally:
-    #  time delay 15 sec
+    #  Time delay 15 sec
     time.sleep(15)
-    # close the browser after all manipulations
+    # Close the browser after all manipulations
     browser.quit()
 
-# don't forget to leave a blank line at the end of the file
+# Don't forget to leave a blank line at the end of the file
