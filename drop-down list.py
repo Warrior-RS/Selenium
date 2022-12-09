0ab3a6f8bef120fd26e0977862f27e52d3879dfc
Anton Klennitski <75129039+Warrior-RS@users.noreply.github.com>
2022-12-09 13:26:51 +0100

    Add files via upload
    http://suninjuly.github.io/selects1.html

---
+++ drop-down list.py
@@ -0,0 +1,33 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def calc(x):
+  return str(int(x)+int(y))        #add up the two values
+
+try:
+    browser = webdriver.Chrome()
+    browser.get('http://suninjuly.github.io/selects1.html')
+
+    first_number = browser.find_element(By.CSS_SELECTOR, "#num1")       #find the value of the first number
+    second_number = browser.find_element(By.CSS_SELECTOR, "#num2")      #find the value of the second number
+
+    x = first_number.text       #The "text" attribute returns the text that is between the opening and closing tags of the element.
+    y = second_number.text
+
+    z = calc(x)     #number to be selected from the list
+
+    input = browser.find_element(By.TAG_NAME, "select")     #"TAG_NAME" searches the text between two tags
+    input.send_keys(z)
+
+    finalclick = browser.find_element(By.TAG_NAME, "button")
+    finalclick.click()
+
+finally:
+
+    time.sleep(10)
+
+    browser.quit()
+
+
