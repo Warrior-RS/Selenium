591699e968d6b83cfbfb485fe8f5b48dfd7ed599
Anton Klennitski <75129039+Warrior-RS@users.noreply.github.com>
2022-12-13 14:45:33 +0100

    Update Downloading files.py
    http://suninjuly.github.io/file_input.html task

--- Downloading files.py
+++ Downloading files.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import os
+##To specify the path to the file, you can use the standard Python module for working with the operating system - os
 
 try:
     link = "http://suninjuly.github.io/file_input.html"
@@ -16,11 +17,11 @@ try:
     element = browser.find_element(By.NAME, "email")
     element.send_keys("Anton@gmail.com")
 
-    current_dir = os.path.abspath(os.path.dirname(__file__))            # получаем путь к директории текущего исполняемого файла
-    file_path = os.path.join(current_dir, 'Downloading files.py')           # добавляем к этому пути имя файла
+    current_dir = os.path.abspath(os.path.dirname(__file__))            # get the path to the directory of the current executable
+    file_path = os.path.join(current_dir, 'Downloading files.py')       # add the file name to this path
 
     choose_file = browser.find_element(By.ID, "file")
-    choose_file.send_keys(file_path)
+    choose_file.send_keys(file_path)                                    ##Send the selected file
 
     finalclick = browser.find_element(By.XPATH, "/html/body/div/form/button")
     finalclick.click()
