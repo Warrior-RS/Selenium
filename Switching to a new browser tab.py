b429f99e2d02b5df538934d178c0cbebc9a9a6be
Anton Klennitski <75129039+Warrior-RS@users.noreply.github.com>
2022-12-15 14:11:17 +0100

    Update Switching to a new browser tab.py

--- Switching to a new browser tab.py
+++ Switching to a new browser tab.py
@@ -16,15 +16,16 @@ try:
     buttom = browser.find_element(By.CSS_SELECTOR, "body > form > div > div > button")
     buttom.click()
 
-    browser.switch_to.window(browser.window_handles[1])
+    browser.switch_to.window(browser.window_handles[1])  #we go to the second tab in the browser
+    # browser.window_handles[0]  We can also remember the name of the current tab to be able to return to it later
     time.sleep(1)
 
 
-    x_element = browser.find_element(By.CSS_SELECTOR, "#input_value")  # забераем значение
+    x_element = browser.find_element(By.CSS_SELECTOR, "#input_value")  # pick up the value
     x = x_element.text
     y = calc(x)
 
-    input = browser.find_element(By.CSS_SELECTOR, "#answer")  # вводим посчитанное значение
+    input = browser.find_element(By.CSS_SELECTOR, "#answer")  # enter the calculated value
     input.send_keys(y)
 
     finalclick = browser.find_element(By.CSS_SELECTOR, "body > form > div > div > button")
